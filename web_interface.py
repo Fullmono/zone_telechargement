16e3ad659fcdef5711f9a0f4a3595c1fe56a88ae
Ricardo Araujo <flintux@gmail.com>
2014-04-21 13:08:15 +0200

    changes in template and added output of command line to webpage when checking new downloads

--- web_interface.py
+++ web_interface.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import subprocess
+import re
 import configparser
 try:
         from flask import Flask, render_template, request, redirect, url_for
@@ -22,54 +24,57 @@ def update_liste(config_file):
                                       'link' : str(config_file[series]['link'])}
         return shows_list
 
+def check_config(configfile):
+        """check if config is ok"""
+        if os.path.isfile(configfile):
+                config = configparser.ConfigParser()
+                config.read(configfile)
+                listeSeries = update_liste(config)
+        else:
+                print ("no config found")
+                config = None
+        return config
 
-if os.path.isfile('config.ini'):
-        configfile = 'config.ini'
-        config = configparser.ConfigParser()
-        config.read(configfile)
-        listeSeries = update_liste(config)
-else:
-        print ("no config found")
-        config = None
+config = check_config('config.ini')
 
+templateData = {}
 
 @app.route("/", methods = ['POST', 'GET'])
 def main():
         """main page display display list of items of no parameters"""
+        global templateData
         listeSeries = update_liste(config)
-        templateData = {
-		'series' : listeSeries
-		}
+        templateData['series'] = listeSeries
         if request.values.get('check_now') == 'check':
                 try:
-                        retcode = os.system('python zone_telechargement.py')
-                        if retcode == 0:
-                                templateData['message'] = 'check réussi'
-                        else:
-                                templateData['message'] = 'check impossible'
-                except OSError as e:
-                        templateData['message'] = 'execution failed' + str(e)
-        if request.values.get('delete') == 'Effacer':
-                templateData['message'] = 'link erased ' + str(request.values.get('name'))
+                        retcode = subprocess.check_output(['python', 'zone_telechargement.py'], universal_newlines=True)
+                        templateData['message'] = str(retcode).split('\n')
+                except subprocess.CalledProcessError as e:
+                        templateData['message'] = str('erreur d\'éxecution\n' + str(e.output)).split('\n')
         return render_template('main.html', **templateData)
 
 
 @app.route("/<serie>", methods = ['POST', 'GET'])
 def edit_form(serie):
         """page for editing info for a specific show"""
+        global templateData
         if serie in config.sections():
                 templateData = {'name' : serie,
                                 'last_episode': str(config[serie]['last_episode']),
                                 'link' : str(config[serie]['link'])}
         else:
-                templateData = {'message' : 'serie ' + str(serie) + ' non trouvée'}
+                templateData = {'message' : ['serie ' + str(serie) + ' non trouvée']}
         if request.values.get('home') == 'Accueil':
+                return redirect(url_for('main'))
+        if request.values.get('delete') == 'Effacer':
+                templateData = {'message' : ['serie ' + str(serie) + ' effacée']}
                 return redirect(url_for('main'))
         return render_template('editserie.html', **templateData)
 
 @app.route("/internal/new", methods = ['POST', 'GET'])
 def new_entry():
         """page for adding a new show to the list"""
+        global templateData
         templateData = {}
         if request.values.get('valider') == 'Valider':
                 templateData = {'name' : str(request.values.get('name')),
@@ -80,8 +85,14 @@ def new_entry():
                 config[str(request.values.get('name'))]['last_episode'] = str(request.values.get('last_episode'))
                 with open('config.ini', 'w') as fichierconfig:
                         config.write(fichierconfig)
-                templateData['message'] = 'lien mis ajour'
+                templateData['message'] = ['lien mis ajour']
         return render_template('newserie.html', **templateData)
+
+@app.route("/internal/config", methods = ['POST', 'GET'])
+def edit_config():
+        global templateData
+        templateData['message'] = ['pas encore implémenté']
+        return redirect(url_for('main'))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8090, debug=True)
